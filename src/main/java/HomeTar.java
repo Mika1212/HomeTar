0ed752a46aad3cdf008e1367fc4b22503434f716
Mika1212 <54846851+Mika1212@users.noreply.github.com>
2021-03-02 16:57:08 +0300

    Command line program

--- src/main/java/HomeTar.java
+++ src/main/java/HomeTar.java
@@ -16,8 +16,54 @@ public class HomeTar {
         String outputName = null;
 
         if (args[1].toLowerCase().equals("-u")) {
+            FileReader reader = new FileReader(args[2]);
+            int character;
+            StringBuilder nameToOutput = new StringBuilder();
+            ArrayList<String> outputFileName = new ArrayList<>();
+            ArrayList<Integer> outputFileNumber = new ArrayList<>();
+            int numberOfLines = 0;
 
-            //
+            while ((character = reader.read()) != -1) {
+                if ((char) character == '\n') {
+                    if (numberOfLines == 1) break;
+                }
+                if ((char) character != '\n') {
+                    nameToOutput.append((char) character);
+                    numberOfLines = 0;
+                } else {
+                    numberOfLines++;
+                    int end = nameToOutput.indexOf(" ");
+                    String name = nameToOutput.substring(0, end);
+                    int number = Integer.parseInt(nameToOutput.substring(end + 1));
+
+                    outputFileName.add(name);
+                    outputFileNumber.add(number);
+                    nameToOutput = new StringBuilder();
+                }
+
+            }
+            reader.read();
+            int i = 0;
+            int k = 0;
+            File newFile = new File(outputFileName.get(i));
+            FileWriter newFileWriter = new FileWriter(newFile);
+            while ((character = reader.read()) != -1) {
+                if (k == 0) {
+                    newFile = new File(outputFileName.get(i));
+                    newFileWriter = new FileWriter(newFile);
+                    i++;
+                }
+                if (k<outputFileNumber.get(i - 1)) {
+                    newFileWriter.write(character);
+                    k++;
+                } else {
+                    newFileWriter.flush();
+                    k = 0;
+                }
+            }
+
+            newFileWriter.flush();
+
 
         } else {
             for (int i = 1; i < args.length - 1; i++) {
@@ -38,37 +84,42 @@ public class HomeTar {
 
                 inputName.add(args[i]);
             }
-        }
 
-        if (outputName == null) {
-            System.out.println("Output file wasn't specified");
-            return;
-        }
-        System.out.println("----------------------\n");
 
-        FileWriter result = new FileWriter(outputName, false);
-        StringBuilder writer = new StringBuilder();
+            if (outputName == null) {
+                System.out.println("Output file wasn't specified");
+                return;
+            }
+            System.out.println("----------------------\n");
 
-        for (String s : inputName) {
-            FileReader reader = new FileReader(s);
-            writer.append("File ").append(inputName.indexOf(s)).append(" ").append(s).append(" {\n");
+            FileWriter result = new FileWriter(outputName, false);
+            StringBuilder writer = new StringBuilder();
+            StringBuilder info = new StringBuilder();
 
-            int character;
-            while ((character = reader.read()) != -1) {
-                writer.append((char) character);
+            for (String s : inputName) {
+                FileReader reader = new FileReader(s);
+                int lengthOfText = 0;
+
+                int character;
+                while ((character = reader.read()) != -1) {
+                    lengthOfText++;
+                    writer.append((char) character);
+                }
+
+                writer.append("\n");
+                info.append(s).append(" ").append(lengthOfText).append("\n");
             }
 
-            writer.append("\n}").append(" File ").append(inputName.indexOf(s)).append(" end\n");
-        }
-        writer.deleteCharAt(writer.length() - 1);
+            writer.deleteCharAt(writer.length() - 1);
+            result.write(info + "\n\n");
+            result.write(String.valueOf(writer));
+            result.flush();
 
-        result.write(String.valueOf(writer));
-        result.flush();
-
-        int character;
-        FileReader resultReader = new FileReader(outputName);
-        while ((character = resultReader.read()) != -1) {
-            System.out.print((char) character);
+            int character;
+            FileReader resultReader = new FileReader(outputName);
+            while ((character = resultReader.read()) != -1) {
+                System.out.print((char) character);
+            }
         }
     }
 }
