b3dc247f1460e14426923829ba3f0369f26e7f4d
Mika1212 <54846851+Mika1212@users.noreply.github.com>
2021-03-11 19:08:20 +0300

    Command line program

--- src/test/java/HomeTarTest.java
+++ src/test/java/HomeTarTest.java
@@ -1,41 +1,68 @@
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 
 import java.io.File;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static java.nio.file.Files.delete;
+import static java.nio.file.Files.readAllBytes;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HomeTarTest {
 
-    public static boolean isEqual(File it, File other) throws IOException {
-        FileReader itReader = new FileReader(it);
-        FileReader otherReader = new FileReader(other);
-        int itCharacter;
-        int otherCharacter;
+    void somePreparations() throws IOException {
+        Path it1 = Paths.get("testData//in1.txt");
+        Path copy1 = Paths.get("testData//inCopy1.txt");
+        Files.copy(it1, copy1, StandardCopyOption.REPLACE_EXISTING);
+        Path it2 = Paths.get("testData//in2.txt");
+        Path copy2 = Paths.get("testData//inCopy2.txt");
+        Files.copy(it2, copy2, StandardCopyOption.REPLACE_EXISTING);
+    }
 
-        while ((itCharacter = itReader.read()) != -1 | (otherCharacter = otherReader.read()) != -1) {
-            if (itCharacter != otherCharacter) {
-                return false;
-            }
+    void ending() throws IOException {
+        Path copy1 = Paths.get("testData//inCopy1.txt");
+        Path copy2 = Paths.get("testData//inCopy2.txt");
+        Path output = Paths.get("testData//output.txt");
+        delete(copy1);
+        delete(copy2);
+        delete(output);
+    }
+
+    public static boolean isEqual(File it, File other) throws IOException {
+
+        byte[] first = readAllBytes(Paths.get(it.getPath()));
+        byte[] second = readAllBytes(Paths.get(other.getPath()));
+        try {
+            assertArrayEquals(first, second);
+        } catch (AssertionFailedError e) {
+            return false;
         }
         return true;
     }
 
     @Test
     void isEqual() throws IOException {
+        somePreparations();
         assertTrue(isEqual(new File("testData//in1.txt"), new File("testData//inCopy1.txt")));
         assertTrue(isEqual(new File("testData//in2.txt"), new File("testData//inCopy2.txt")));
         assertFalse(isEqual(new File("testData//inCopy1.txt"), new File("testData//inCopy2.txt")));
+        ending();
     }
 
     @Test
     void main() throws IOException {
+        somePreparations();
         HomeTar.main(new String[]{"hometar", "testData//in1.txt", "testData//in2.txt", "-out", "testData//output.txt"});
         HomeTar.main(new String[]{"hometar", "-u", "testData//output.txt"});
         assertTrue(isEqual(new File("testData//in1.txt"), new File("testData//inCopy1.txt")));
         assertTrue(isEqual(new File("testData//in2.txt"), new File("testData//inCopy2.txt")));
+        ending();
     }
 
 
